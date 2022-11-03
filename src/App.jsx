25e2d1817696baf26a47e19b00a7dbc4e41ab63d
dhanjuma <abdullahyahaya212@gmail.com>
2022-11-03 16:39:55 -0700

    fixed import

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import HomePage from "./pages/home";
+import HomePage from "./pages/HomePage";
 import Contact from "./pages/Contact";
 import Footer from "./components/footer";
 
